d7dc5a7f916b76b7cb420240c4d76d88872abc1c
Ahmad Ibrahim <aburabee3@Ahmads-Mac-mini.local>
2023-08-16 21:16:05 -0600

    first page complete mobile

--- js/header.js
+++ js/header.js
@@ -1,13 +1,51 @@
-let prevScrollPos = window.scrollY || window.pageYOffset;
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  'use strict';
+  let savedScrollY = 0;
+  // Function to save the current scroll position
+function saveScrollPosition() {
+  savedScrollY = window.scrollY;
+}
+
+// Function to restore the saved scroll position
+function restoreScrollPosition() {
+  window.scrollTo(0, savedScrollY);
+}
+  let header = document.querySelector(".header");
+  var link = document.querySelector('[data-toggle-menu]');
+  var dropDown = document.querySelector('.dropDown');
+  link.addEventListener('click', function() {
+    if (link.classList.contains('toggle-menu--clicked')) {
+      link.classList.remove('toggle-menu--clicked');
+      dropDown.style.top = "-1000px";
+      header.classList.remove('stick')
+      restoreScrollPosition();
+    } else {
+      link.classList.add('toggle-menu--clicked');
+      dropDown.style.top = "0";
+      header.classList.add('stick')
+      saveScrollPosition();
+    }
+  }, false);
+
+
+  let prevScrollPos = window.scrollY || window.pageYOffset;
 
 window.addEventListener("scroll", () => {
   const currentScrollPos = window.scrollY || window.pageYOffset;
-
-  if (prevScrollPos > currentScrollPos) {
-    document.querySelector(".header").style.top = "0";
+  if (prevScrollPos > currentScrollPos || currentScrollPos <= 0) {
+    header.style.top = "0";
   } else {
-    document.querySelector(".header").style.top = "-300px";
+    if(!header.classList.contains("stick")){
+    header.style.top = "-300px";
+    }
   }
 
   prevScrollPos = currentScrollPos;
-});
+}); 
+
+
+
+}, false);
